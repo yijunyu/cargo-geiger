d535fbf9381553507b2754c35c92c5a3a24fd537
Yijun Yu <y.yu@open.ac.uk>
2021-01-23 21:40:09 +0000

    update

--- bin/gitpod.sh
+++ bin/gitpod.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
+REPO=$(pwd)
 cd /workspace > /dev/null
 git clone https://github.com/yijunyu/cargo-geiger
 cd cargo-geiger > /dev/null
 git checkout datasets
 mkdir -p /workspace/.cargo/bin
 cp bin/* /workspace/.cargo/bin
-cd - > /dev/null
+cd $REPO > /dev/null
 saferatio.sh
